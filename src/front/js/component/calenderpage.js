fc09c38d770d4249e00f527490b41c625d22a777
andyymor <61852990+andyymor@users.noreply.github.com>
2024-03-01 01:58:29 +0000

    Co-authored-by: Valerie Dubach <Valerieclaire96@users.noreply.github.com> Fixed the calender page link, need to fix the date time picker, and add edit flight options

--- src/front/js/component/calenderpage.js
+++ src/front/js/component/calenderpage.js
@@ -5,9 +5,10 @@ import startOfWeek from "date-fns/startOfWeek";
 import React, { useState } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import DatePicker from "react-datepicker";
+import DateTimePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 // import "./App.css";
+
 
 const locales = {
     "en-US": require("date-fns/locale/en-US"),
@@ -35,32 +36,31 @@ export function Thecalender() {
     const [allFlights, setAllFlights] = useState(events);
 
     function handleAddEvent() {
-        
-        for (let i=0; i<allFlights.length; i++){
 
-            const d1 = new Date (allFlights[i].start);
+        for (let i = 0; i < allFlights.length; i++) {
+
+            const d1 = new Date(allFlights[i].start);
             const d2 = new Date(newFlight.start);
             const d3 = new Date(allFlights[i].end);
             const d4 = new Date(newFlight.end);
-      /*
-          console.log(d1 <= d2);
-          console.log(d2 <= d3);
-          console.log(d1 <= d4);
-          console.log(d4 <= d3);
-            */
+            /*
+                console.log(d1 <= d2);
+                console.log(d2 <= d3);
+                console.log(d1 <= d4);
+                console.log(d4 <= d3);
+                  */
 
-             if (
-              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
-                (d4 <= d3) )
-              )
-            {   
-                alert("CLASH"); 
+            if (
+                ((d1 <= d2) && (d2 <= d3)) || ((d1 <= d4) &&
+                    (d4 <= d3))
+            ) {
+                alert("CLASH");
                 break;
-             }
-    
+            }
+
         }
-        
-        
+
+
         setAllFlights([...allFlights, newFlight]);
     }
 
@@ -69,9 +69,9 @@ export function Thecalender() {
             <h1>Calendar</h1>
             <h2>Add New Event</h2>
             <div>
-                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newFlight.title} onChange={(e) => setnewFlight({ ...newFlight, title: e.target.value })} />
-                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newFlight.start} onChange={(start) => setnewFlight({ ...newFlight, start })} />
-                <DatePicker placeholderText="End Date" selected={newFlight.end} onChange={(end) => setnewFlight({ ...newFlight, end })} />
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newFlight.title} onChange={(e) => setNewFlight({ ...newFlight, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newFlight.start} onChange={(start) => setNewFlight({ ...newFlight, start })} />
+                <DatePicker placeholderText="End Date" selected={newFlight.end} onChange={(end) => setNewFlight({ ...newFlight, end })} />
                 <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
                     Add Event
                 </button>
